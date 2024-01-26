43714ef12adc7043e4bd163c2f2ac2a19e866208
Hayden Schroeder <hschroeder0317@gmail.com>
2024-01-26 11:02:50 -0600

    add count

--- blink.py
+++ blink.py
@@ -1,17 +1,23 @@
+import argparse
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep from time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 
-ITER_COUNT = 15  
 pin1 = 13
 
-GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)   
+parser = argparse.ArgumentParser(
+    prog='program.py', description='Blink LED for a specified time and interval', epilog='Enjoy the program!')
+parser.add_argument('count', type=int)
+args = parser.parse_args()
+count = args.count
 
-while ITER_COUNT > 0: # Run ITER_COUNT times
-   ITER_COUNT -= 1 # Decrement counter
-   GPIO.output(pin1, GPIO.HIGH) # Turn on
-   sleep(1)                     # Sleep for 1 second
-   GPIO.output(pin1, GPIO.LOW)  # Turn off
-   sleep(1)                     # Sleep for 1 second
+GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)
+
+while count > 0:  # Run ITER_COUNT times
+    count -= 1  # Decrement counter
+    GPIO.output(pin1, GPIO.HIGH)  # Turn on
+    sleep(1)                     # Sleep for 1 second
+    GPIO.output(pin1, GPIO.LOW)  # Turn off
+    sleep(1)                     # Sleep for 1 second
 GPIO.cleanup()
