18d29f82e57890431e74c44ceac45f5f8724667d
Hayden Schroeder <hschroeder0317@gmail.com>
2024-01-26 11:08:49 -0600

    add default

--- blink.py
+++ blink.py
@@ -4,7 +4,8 @@ from time import sleep     # Import the sleep from time module
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 
-pin1 = 13
+OUTPUT_PIN = 13
+INPUT_PIN = 11
 
 parser = argparse.ArgumentParser(
     prog='program.py', description='Blink LED for a specified time and interval', epilog='Enjoy the program!')
@@ -12,12 +13,14 @@ parser.add_argument('count', type=int, default=5, nargs='?')
 args = parser.parse_args()
 count = args.count
 
-GPIO.setup(pin1, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(OUTPUT_PIN, GPIO.OUT, initial=GPIO.LOW)
+GPIO.setup(INPUT_PIN, GPIO.IN)
 
-while count > 0:  # Run ITER_COUNT times
-    count -= 1  # Decrement counter
-    GPIO.output(pin1, GPIO.HIGH)  # Turn on
-    sleep(1)                     # Sleep for 1 second
-    GPIO.output(pin1, GPIO.LOW)  # Turn off
-    sleep(1)                     # Sleep for 1 second
+if GPIO.input(INPUT_PIN) == 1:
+    while count > 0:  # Run ITER_COUNT times
+        count -= 1  # Decrement counter
+        GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Turn on
+        sleep(1)                     # Sleep for 1 second
+        GPIO.output(OUTPUT_PIN, GPIO.LOW)  # Turn off
+        sleep(1)                     # Sleep for 1 second
 GPIO.cleanup()
