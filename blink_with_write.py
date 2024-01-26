34a03e08b7d2ad22bfcedd25e0483adf7795ac8a
Hayden Schroeder <hschroeder0317@gmail.com>
2024-01-26 11:38:17 -0600

    added time

--- blink_with_write.py
+++ blink_with_write.py
@@ -1,17 +1,15 @@
 import argparse
 import RPi.GPIO as GPIO    # Import Raspberry Pi GPIO library
 from time import sleep     # Import the sleep from time module
+from time import time
+
 GPIO.setwarnings(False)    # Ignore warning for now
 GPIO.setmode(GPIO.BOARD)   # Use physical pin numbering
 
 OUTPUT_PIN = 13
 INPUT_PIN = 11
 
-# parser = argparse.ArgumentParser(
-#     prog='program.py', description='Blink LED for a specified time and interval', epilog='Enjoy the program!')
-# parser.add_argument('count', type=int, default=5, nargs='?')
-# args = parser.parse_args()
-# count = args.count
+start_time = time()
 
 
 def write_to_file(msg):
@@ -25,7 +23,7 @@ GPIO.setup(INPUT_PIN, GPIO.IN)
 
 while True:  # Run ITER_COUNT times
     input_pin = GPIO.input(INPUT_PIN)
-    write_to_file(f'input_pin = {input_pin}')
+    write_to_file(f'{time() - start_time}:\tinput_pin = {input_pin}')
     while (input_pin == GPIO.LOW):
         GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Turn on
         sleep(1)                     # Sleep for 1 second
