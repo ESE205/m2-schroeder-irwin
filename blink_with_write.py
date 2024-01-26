bd8e5d7e0bc15402ee89d131130f2d0aeee36fe8
Hayden Schroeder <hschroeder0317@gmail.com>
2024-01-26 11:49:21 -0600

    add args

--- blink_with_write.py
+++ blink_with_write.py
@@ -19,8 +19,12 @@ parser = argparse.ArgumentParser(
     prog='program.py', description='Blink LED for a specified time and interval', epilog='Enjoy the program!')
 
 parser.add_argument('-d', '--debug', action='store_true')
+parser.add_argument('run_time', type=int)
+parser.add_argument('blink_interval', type=int)
 args = parser.parse_args()
 debug = args.debug
+run_time = args.run_time
+blink_interval = args.blink_interval
 
 start_time = time()
 
@@ -34,14 +38,14 @@ GPIO.setup(OUTPUT_PIN, GPIO.OUT, initial=GPIO.LOW)
 GPIO.setup(INPUT_PIN, GPIO.IN)
 
 
-while True:  # Run ITER_COUNT times
+while time() - start_time < run_time:  # Run ITER_COUNT times
     input_pin = GPIO.input(INPUT_PIN)
     write_to_file(f'{time() - start_time}:\tinput_pin = {input_pin}')
     print_debug(f'{time() - start_time}:\tinput_pin = {input_pin}')
     if (input_pin == GPIO.LOW):
         GPIO.output(OUTPUT_PIN, GPIO.HIGH)  # Turn on
-        sleep(1)                     # Sleep for 1 second
+        sleep(blink_interval)
         GPIO.output(OUTPUT_PIN, GPIO.LOW)  # Turn off
-        sleep(1)                     # Sleep for 1 second
+        sleep(blink_interval)
 
 GPIO.cleanup()
